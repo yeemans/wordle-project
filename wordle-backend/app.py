3aa58f3642b38d54d1f69afb3a62cd90723cf8c0
yeemans <arthur.roth.07@gmail.com>
2026-04-30 14:29:10 -0700

    added get_color function to backend

--- wordle-backend/app.py
+++ wordle-backend/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 answer = ""
+WORD_LENGTH = 5
+COLORS = ["green", "gray", "yellow"]
 
 # Choose an answer word when server starts
 with open("possible-answers.txt", "r") as f:
@@ -11,7 +15,33 @@ with open("possible-answers.txt", "r") as f:
 
 @app.route("/get-answer", methods=["GET"])
 def get_answers():
+    print(answer)
     return jsonify({"answer": answer})
+
+# return colors for each box for a guess
+@app.route("/get-box-colors", methods=["POST"])
+def get_box_colors():
+    data = request.get_json()
+    word = data.get("word")
+    box_colors = ["gray", "gray", "gray", "gray", "gray"]
+    
+    print("Received word:", word)
+
+    for i in range(WORD_LENGTH):
+        # letter is in correct slot
+        if word[i] == answer[i]:
+            box_colors[i] = "green"
+        
+        # letter is in wrong slot, but still in word
+        if word[i] != answer[i] and word[i] in answer:
+            box_colors[i] = "yellow"
+
+    return {"box_colors": box_colors}
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
