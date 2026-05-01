20d90da94b28159ea9ee810cbb15da62c46be9c6
yeemans <arthur.roth.07@gmail.com>
2026-04-30 20:26:44 -0700

    ensured guesses are in the list of words

--- wordle-backend/app.py
+++ wordle-backend/app.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 answer = ""
+allowed_guesses = set()
+
 WORD_LENGTH = 5
 COLORS = ["green", "gray", "yellow"]
 
@@ -12,6 +14,12 @@ COLORS = ["green", "gray", "yellow"]
 with open("possible-answers.txt", "r") as f:
     WORDS = [line.strip() for line in f if line.strip()]
     answer = random.choice(WORDS)
+
+# load possible guesses into a set
+with open("allowed-guesses.txt", "r") as f:
+    allowed_guesses = set([line.strip() for line in f if line.strip()])
+
+
 
 @app.route("/get-answer", methods=["GET"])
 def get_answers():
@@ -21,11 +29,10 @@ def get_answers():
 # return colors for each box for a guess
 @app.route("/get-box-colors", methods=["POST"])
 def get_box_colors():
+    valid = True
     data = request.get_json()
     word = data.get("word")
     box_colors = ["gray", "gray", "gray", "gray", "gray"]
-    
-    print("Received word:", word)
 
     for i in range(WORD_LENGTH):
         # letter is in correct slot
@@ -36,12 +43,8 @@ def get_box_colors():
         if word[i] != answer[i] and word[i] in answer:
             box_colors[i] = "yellow"
 
-    return {"box_colors": box_colors}
-
-
-
-
-
+    valid = len(word) == 5 and word in allowed_guesses
+    return {"valid": valid, "box_colors": box_colors}
 
 if __name__ == "__main__":
     app.run(debug=True)
