b725e7389081581093273d6097d93ee481e06a5a
yeemans <arthur.roth.07@gmail.com>
2026-04-30 20:18:55 -0700

    remove __pycache__ from tracking

--- wordle-frontend/src/App.tsx
+++ wordle-frontend/src/App.tsx
@@ -6,8 +6,11 @@ import "./App.css";
 function App() {
   const ROWS = 6;
   const COLS = 5;
+  const [currentRow, setCurrentRow] = useState(0);
+  const [currentCol, setCurrentCol] = useState(0);
+
   type BoxColorsResponse = {
-  colors: ("green" | "gray" | "yellow")[];
+    box_colors: ("green" | "gray" | "yellow")[];
   };
 
   type Cell = {
@@ -32,6 +35,69 @@ function App() {
 
     return res.json();
   };
+
+  useEffect(() => {
+    const handleKey = async (e: KeyboardEvent) => {
+      const key = e.key.toLowerCase();
+
+      // backspace
+      if (key === "backspace") {
+        if (currentCol === 0) return;
+
+        // copy grid, modify copied grid, then setGrid
+        const newGrid = grid.map(r => [...r]);
+        newGrid[currentRow][currentCol - 1] = { letter: "", status: "" };
+
+        setGrid(newGrid);
+        setCurrentCol(c => c - 1);
+        return;
+      }
+
+      // submit guess
+      if (key === "enter") {
+        if (currentCol < COLS) return;
+
+        const guess = grid[currentRow].map(c => c.letter).join("");
+
+        try {
+
+          const data = await getBoxColors(guess);
+          console.log(data);
+          const newGrid = grid.map(r => [...r]);
+
+          newGrid[currentRow] = newGrid[currentRow].map((cell, i) => ({
+            ...cell,
+            status: data.box_colors[i],
+          }));
+
+          setGrid(newGrid);
+          setCurrentRow(r => r + 1); // move to next row
+          setCurrentCol(0);
+        } catch (error) {
+          console.error("API error:", error);
+        }
+        return;
+      }
+
+      // typing letters
+      if (key.length === 1 && key >= "a" && key <= "z") {
+        if (currentCol >= COLS) return;
+
+        const newGrid = grid.map(r => [...r]);
+        newGrid[currentRow][currentCol] = {
+          letter: key,
+          status: "",
+        };
+
+        setGrid(newGrid);
+        setCurrentCol(c => c + 1);
+      }
+    };
+
+    window.addEventListener("keydown", handleKey);
+    // cleanup
+    return () => window.removeEventListener("keydown", handleKey);
+  }, [grid, currentRow, currentCol]);
 
   return (
     <>
